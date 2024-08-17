f9beefc16bfd4cfce78639efc31da6584693d8ed
Kristyna Petrlikova <petrlik@matfyz.cz>
2024-08-17 02:20:03 +0200

    add basic parsing

---
+++ src/LuaTypes.hs
@@ -0,0 +1,131 @@
+{-# LANGUAGE InstanceSigs #-}
+module LuaTypes where
+
+import qualified Data.Int
+import Data.Map
+import Prelude hiding (lookup)
+import System.Environment ()
+
+-- TODO: object store? Address -> Value
+type Identifier = String
+
+data Env
+
+-- AST is based on https://www.lua.org/manual/5.4/manual.html#9
+data Numeric
+  = Integer Data.Int.Int64
+  | Double Double
+  deriving (Show)
+
+instance Ord Numeric where
+  Integer a <= Integer b = a <= b
+  Integer a <= Double b = fromIntegral a <= b
+  Double a <= Integer b = a <= fromIntegral b
+  Double a <= Double b = a <= b
+
+instance Eq Numeric where
+  Integer a == Integer b = a == b
+  Integer a == Double b = fromIntegral a == b
+  Double a == Integer b = a == fromIntegral b
+  Double a == Double b = a == b
+
+data Table = TableData
+  { getTable :: Map Value Value
+  , getMetatable :: Maybe Table
+  } deriving (Show, Eq, Ord)
+
+data Value = Value
+  { getReferenceId :: Maybe Int
+  , getValue :: Type
+  } deriving (Show, Ord)
+
+data Type
+  = Nil
+  | Number Numeric
+  | Boolean Bool
+  | String String
+  | Function
+      { getEnv :: () -- TODO
+      , getArguments :: [Int]
+      , getBody :: Block
+      }
+  | Table Table
+  -- | Userdata
+  -- | Thread
+  deriving (Show, Eq, Ord)
+
+instance Eq Value where
+  (==) :: Value -> Value -> Bool
+  (Value _ Nil) == (Value _ Nil) = True
+  (Value _ (Number v1)) == (Value _ (Number v2)) = v1 == v2
+  (Value _ (Boolean b1)) == (Value _ (Boolean b2)) = b1 == b2
+  (Value _ (String s1)) == (Value _ (String s2)) = s1 == s2
+  (Value id1 (Function {})) == (Value id2 (Function {})) = id1 == id2
+  (Value id1 (Table t1)) == (Value id2 (Table t2)) =
+    case (t1, t2) of
+      (TableData r1 mt1, TableData r2 mt2) -> (id1 == id2) -- TODO
+  _ == _ = False
+
+data Lhs
+  = LIdent Identifier
+  | LIndex Expr Expr
+  deriving (Show, Eq, Ord)
+
+data Statement
+  = Assignment [Lhs] [Expr]
+  | Call Identifier [Expr]
+  | Break
+  | Do Block
+  | While Expr Block
+  | RepeatUntil Expr Block
+  | If Expr Block (Maybe [(Expr, Block)]) (Maybe Block)
+  | Local [Identifier] (Maybe [Expr])
+  | ForNum Expr Expr (Maybe Expr) Block
+  | ForIn [Identifier] [Expr] Block
+  -- | Label String
+  -- | Goto String
+  deriving (Show, Eq, Ord)
+
+data Block = Block
+  { getStatements :: [Statement]
+  , getReturn :: Maybe [Expr]
+  } deriving (Show, Eq, Ord)
+
+data Expr
+  = EType Type
+  | EBinOp BinOp Expr Expr
+  | EUnOp UnOp Expr
+  | EVar Identifier
+  | ECall Identifier [Expr]
+  | EPar Expr
+  | ELhs Lhs
+  | EDots
+  deriving (Show, Eq, Ord)
+
+data BinOp
+  = Add
+  | Sub
+  | Mul
+  | Div
+  | FloorDiv
+  | Pow
+  | Mod
+  | Concat
+  | Eq
+  | Lt
+  | Le
+  | And
+  | Or
+  | BitwiseAnd
+  | BitwiseOr
+  | Xor
+  | Lshift
+  | Rshift
+  deriving (Show, Eq, Ord)
+
+data UnOp
+  = Minus
+  | Not
+  | Len
+  | Neg
+  deriving (Show, Eq, Ord)
---
+++ src/Parsing.hs
@@ -0,0 +1,197 @@
+{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE InstanceSigs #-}
+
+module Parsing where
+
+import Control.Applicative
+import Data.Char
+import qualified Data.Int as Data.Integer
+import Data.Maybe
+import qualified GHC.Enum as Int64
+import LuaTypes
+
+type Int64 = Data.Integer.Int64
+
+data Input = Input
+  { inputLoc :: Int
+  , inputStr :: String
+  } deriving (Show, Eq)
+
+inputHead :: Input -> Maybe (Char, Input)
+inputHead (Input _ []) = Nothing
+inputHead (Input loc (x:xs)) = Just (x, Input (loc + inc) xs)
+  where
+    inc =
+      if x == '\n'
+        then 1
+        else 0
+
+data ParserError =
+  ParserError Int String
+  deriving (Show)
+
+newtype Parser a = Parser
+  { runParser :: Input -> Either ParserError (a, Input)
+  }
+
+instance Functor Parser where
+  fmap :: (a -> b) -> Parser a -> Parser b
+  fmap f (Parser p) =
+    Parser $ \input -> do
+      (a', input') <- p input
+      pure (f a', input')
+
+instance Applicative Parser where
+  pure :: a -> Parser a
+  pure x = Parser $ \input -> Right (x, input)
+  (Parser p1) <*> (Parser p2) =
+    Parser $ \input -> do
+      (f, input') <- p1 input
+      (a, input'') <- p2 input'
+      pure (f a, input'')
+
+instance Alternative (Either ParserError) where
+  empty = Left $ ParserError 1 "empty"
+  Left _ <|> e2 = e2
+  e1 <|> _ = e1
+
+instance Alternative Parser where
+  empty = Parser $ const empty
+  (Parser p1) <|> (Parser p2) = Parser $ \input -> p1 input <|> p2 input
+
+charP :: Char -> Parser Char
+charP x = Parser f
+  where
+    f input@(inputHead -> Just (y, ys))
+      | y == x = Right (x, ys)
+      | otherwise =
+        Left
+          $ ParserError
+              (inputLoc input)
+              ("Expected '" ++ [x] ++ "', but found '" ++ [y] ++ "'")
+    f input =
+      Left
+        $ ParserError
+            (inputLoc input)
+            ("Expected '" ++ [x] ++ "', but reached end of string")
+
+stringP :: String -> Parser String
+stringP str =
+  Parser $ \input ->
+    case runParser (traverse charP str) input of
+      Left _ ->
+        Left
+          $ ParserError
+              (inputLoc input)
+              ("Expected \""
+                 ++ str
+                 ++ "\", but found \""
+                 ++ inputStr input
+                 ++ "\'")
+      result -> result
+
+parseIf :: String -> (Char -> Bool) -> Parser Char
+parseIf desc f =
+  Parser $ \input ->
+    case input of
+      (inputHead -> Just (y, ys))
+        | f y -> Right (y, ys)
+        | otherwise ->
+          Left
+            $ ParserError
+                (inputLoc input)
+                ("Expected " ++ desc ++ ", but found '" ++ [y] ++ "'")
+      _ ->
+        Left
+          $ ParserError
+              (inputLoc input)
+              ("Expected " ++ desc ++ ", but reached end of string")
+
+spanP :: String -> (Char -> Bool) -> Parser String
+spanP desc = many . parseIf desc
+
+span1P :: String -> (Char -> Bool) -> Parser String
+span1P desc = some . parseIf desc
+
+ws :: Parser String
+ws = spanP "whitespace character" isSpace
+
+luaNil :: Parser Expr
+luaNil = EType Nil <$ stringP "nil"
+
+luaBool :: Parser Expr
+luaBool = luaTrue <|> luaFalse
+  where
+    luaTrue = EType (Boolean True) <$ stringP "true"
+    luaFalse = EType (Boolean False) <$ stringP "false"
+
+luaNumber :: Parser Expr
+luaNumber = EType . Number <$> (numberHex <|> numberDec)
+
+option :: a -> Parser a -> Parser a
+option defVal p = p <|> pure defVal
+
+digits1 :: Parser Int64
+digits1 = read <$> span1P "digit" isDigit
+
+hexDigits1 :: Parser Int64
+hexDigits1 = read . (\val -> '0' : 'x' : val) <$> span1P "hex digit" isHexDigit
+
+minus :: Parser Int64
+minus = (-1) <$ charP '-'
+
+optMinus :: Parser Int64
+optMinus = option 1 minus
+
+plus :: Parser Int64
+plus = 1 <$ charP '+'
+
+optSign :: Parser Int64
+optSign = option 1 (plus <|> minus)
+
+numberDec :: Parser Numeric
+numberDec =
+  partsToNum 10 10
+    <$> optMinus -- sign 
+    <* ws
+    <*> digits1 -- decimal part
+    <*> optional (charP '.' *> option 0 digits1) -- fractional part
+    <*> optional
+          ((charP 'e' <|> charP 'E') *> ((*) <$> optSign <*> option 0 digits1))
+
+numberHex :: Parser Numeric
+numberHex =
+  partsToNum 16 2
+    <$> optMinus -- sign 
+    <* (ws <* (stringP "0x" <|> stringP "0X"))
+    <*> hexDigits1 -- decimal part
+    <*> optional (charP '.' *> option 0 hexDigits1) -- fractional part
+    <*> optional
+          ((charP 'p' <|> charP 'P')
+             *> ((*) <$> optSign <*> option 0 hexDigits1))
+
+partsToNum ::
+     Double -- fractional base
+  -> Double -- exponent base
+  -> Int64 -- sign 
+  -> Int64 -- decimal part
+  -> Maybe Int64 -- fractional part
+  -> Maybe Int64 -- exponent part
+  -> Numeric
+partsToNum fbase ebase sign dec frac exp' =
+  if isNothing frac && isNothing exp'
+    then Integer $ sign * dec
+    else Double res
+  where
+    combineFrac :: Int64 -> Maybe Int64 -> Double
+    combineFrac a (Just b) =
+      fromIntegral a
+        + (fromIntegral b / (fbase ^^ fromIntegral (length (show b))))
+    combineFrac a Nothing = fromIntegral a
+    combineExp :: Double -> Maybe Int64 -> Double
+    combineExp a (Just b) = a * ebase ^^ b
+    combineExp a Nothing = a
+    res :: Double
+    res =
+      combineExp (fromIntegral sign * combineFrac (fromIntegral dec) frac) exp'
